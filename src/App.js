2374b966c1b6a747d836a5c17e45d7b6df37a77d
IamNatetrizton <taiwotrizton@gmail.com>
2022-12-15 14:48:31 +0100

    react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import React from 'react';
+
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      
+      <div className='content'>
+      <h1>App component</h1>
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
